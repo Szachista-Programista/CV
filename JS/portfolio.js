5c8ae51034882bbcf8953e17f42afa5e2c4056a2
Szachista-Programista <krzysztofkazanecki6@gmail.com>
2024-03-07 11:50:08 +0100

    Minor styling adjustments

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,25 +1,4 @@
 /////////////////////SCROLLBAR//////////////////////
-/*document.addEventListener("DOMContentLoaded", function () {
-    document.documentElement.scrollTop = 270;
-});*/
-////////////////////////////////////////////////////
-document
-  .getElementById("scrollLink1")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    window.scrollTo(0, 1600);
-  });
-
-document
-  .getElementById("scrollLink2")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    window.scrollTo(0, 2900);
-  });
-
-document
-  .getElementById("scrollLink3")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    window.scrollTo(0, 4200);
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  document.documentElement.scrollTop = 110;
+});
