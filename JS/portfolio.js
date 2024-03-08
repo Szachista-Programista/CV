0a77e5f1a5827a62042995fe1c8ea943eba682d2
Szachista-Programista <krzysztofkazanecki6@gmail.com>
2024-03-08 14:45:49 +0100

    add portflolio new content

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -2,29 +2,3 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.documentElement.scrollTop = 110;
 });
-<<<<<<< HEAD
-=======
-////////////////////////////////////////////////////
-/*
-document
-  .getElementById("scrollLink1")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    window.scrollTo(0, 1600);
-  });
-
-document
-  .getElementById("scrollLink2")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    window.scrollTo(0, 2900);
-  });
-
-document
-  .getElementById("scrollLink3")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    window.scrollTo(0, 4200);
-  });
-*/
->>>>>>> 3f67e76fce272e606c524231729072bcdf5f2f08
